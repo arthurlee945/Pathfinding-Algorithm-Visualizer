a6f15dc12f5b10d3868420de94d06457f647c361
Arthur Lee <arthur.lee945@gmail.com>
2023-07-08 17:56:07 -0500

    update

--- Assets/Scripts/UI/OptionsDropdown.cs
+++ Assets/Scripts/UI/OptionsDropdown.cs
@@ -2,17 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(RectTransform))]
 public class OptionsDropdown : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] RectTransform optDropdown;
+    [SerializeField] float dropdownSpeed = 650f;
+    RectTransform rectTransform;
+    float maxHeight = 125f;
+    float dropdownWidth;
+    bool isOpen;
+    Coroutine dropdownAnimationCoroutine;
+    void Awake()
     {
-
+        rectTransform = GetComponent<RectTransform>();
+        maxHeight = rectTransform.sizeDelta.y;
+        dropdownWidth = rectTransform.sizeDelta.x;
+        rectTransform.sizeDelta = new Vector2(dropdownWidth, optDropdown.sizeDelta.y);
     }
-
-    // Update is called once per frame
-    void Update()
+    public void HandleOptionsDropdownToggle()
     {
-
+        if (dropdownAnimationCoroutine != null)
+            StopCoroutine(dropdownAnimationCoroutine);
+        isOpen = !isOpen;
+        dropdownAnimationCoroutine = StartCoroutine(DropdownAnimation(isOpen));
+    }
+    IEnumerator DropdownAnimation(bool open)
+    {
+        bool criteria = open ? rectTransform.sizeDelta.y <= maxHeight : rectTransform.sizeDelta.y >= optDropdown.sizeDelta.y;
+        while (criteria)
+        {
+            rectTransform.sizeDelta = new Vector2(dropdownWidth, rectTransform.sizeDelta.y + ((open ? 1 : -1) * dropdownSpeed * Time.deltaTime));
+            criteria = open ? rectTransform.sizeDelta.y <= maxHeight : rectTransform.sizeDelta.y >= optDropdown.sizeDelta.y;
+            yield return new WaitForEndOfFrame();
+        }
     }
 }
---
+++ Assets/Scripts/UI/PointerEnterEventHandler.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PointerEnterEventHandler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] Texture2D cursorTexture;
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Cursor.SetCursor(cursorTexture, new Vector2(16, 0), CursorMode.Auto);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+    }
+}
