f092086077c9507f007441f88b8725d70cdde8c1
Arthur Lee <arthur.lee945@gmail.com>
2023-07-09 20:02:13 -0500

    ud

--- Assets/Scripts/Manager/InputManager.cs
+++ Assets/Scripts/Manager/InputManager.cs
@@ -5,10 +5,16 @@ using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
-    [SerializeField] Camera cam;
+    [SerializeField] Camera mainCamera;
+    [Header("Camera Movment Fields")]
     [SerializeField] InputAction cameraMovement;
+    [Header("Camera Rotation Fields")]
+    [SerializeField] float smoothInputSpeed = .3f;
     [SerializeField] InputAction cameraRotation;
+    [Header("Camera Drag Fields")]
     [SerializeField] InputAction cameraDrag;
+    Vector2 currentInputMovementVector;
+    Vector2 smoothInputVelocity;
     private void OnEnable()
     {
         cameraMovement.Enable();
@@ -19,6 +25,15 @@ public class InputManager : MonoBehaviour
     }
     void Update()
     {
-        // cameraControls.ReadValue<Vector2>();
+        HanldeCameraMovement();
+    }
+
+    private void HanldeCameraMovement()
+    {
+        Vector2 movement = cameraMovement.ReadValue<Vector2>();
+        if (movement.x == 0 && movement.y == 0) return;
+        currentInputMovementVector = Vector2.SmoothDamp(currentInputMovementVector, movement, ref smoothInputVelocity, smoothInputSpeed);
+        // mainCamera.transform.localPosition = new Vector3()
+
     }
 }
