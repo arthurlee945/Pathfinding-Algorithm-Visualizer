83217a1743fe1d90e122ba5506d55caceed86825
Arthur Lee <arthur.lee945@gmail.com>
2023-07-09 21:43:54 -0500

    adding camera control

--- Assets/Scripts/Manager/InputManager.cs
+++ Assets/Scripts/Manager/InputManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,15 +7,19 @@ using UnityEngine.InputSystem;
 public class InputManager : MonoBehaviour
 {
     [SerializeField] Camera mainCamera;
+    //-------------Movement fields
     [Header("Camera Movment Fields")]
     [SerializeField] InputAction cameraMovement;
-    [Header("Camera Rotation Fields")]
+    [SerializeField] InputAction movementSpeedUp;
     [SerializeField] float smoothInputSpeed = .3f;
-    [SerializeField] InputAction cameraRotation;
-    [Header("Camera Drag Fields")]
-    [SerializeField] InputAction cameraDrag;
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float fastMoveSpeed = 20f;
     Vector2 currentInputMovementVector;
     Vector2 smoothInputVelocity;
+    //-------------Drag fields
+    Vector2 startingDragPos;
+    Vector2 cameraInitPos;
+
     private void OnEnable()
     {
         cameraMovement.Enable();
@@ -23,17 +28,52 @@ public class InputManager : MonoBehaviour
     {
         cameraMovement.Disable();
     }
+    void Awake()
+    {
+        if (mainCamera == null)
+        {
+            mainCamera = Camera.main;
+        }
+    }
     void Update()
     {
-        HanldeCameraMovement();
+        if (Mouse.current.rightButton.isPressed)
+        {
+            HanldeCameraMovement();
+        }
+        else if (Mouse.current.middleButton.isPressed)
+        {
+            HandleCameraDrag();
+        }
+        else
+        {
+            ResetCameraInput();
+        }
+    }
+
+    private void ResetCameraInput()
+    {
+        if (currentInputMovementVector.x != 0 || currentInputMovementVector.y != 0)
+            currentInputMovementVector = Vector2.zero;
+    }
+
+    private void HandleCameraDrag()
+    {
+        Vector2 currMousePos = Mouse.current.position.ReadValue();
+        if (Mouse.current.middleButton.wasPressedThisFrame)
+        {
+            startingDragPos = currMousePos;
+            cameraInitPos = new Vector2(mainCamera.transform.position.x, mainCamera.transform.position.y);
+        }
+        Vector2 differentiatedPos = currMousePos - startingDragPos;
+        mainCamera.transform.localPosition = new Vector3(cameraInitPos.x + (differentiatedPos.x / 2), cameraInitPos.y + (differentiatedPos.y / 2), mainCamera.transform.position.z);
     }
 
     private void HanldeCameraMovement()
     {
         Vector2 movement = cameraMovement.ReadValue<Vector2>();
-        if (movement.x == 0 && movement.y == 0) return;
+        if (currentInputMovementVector.x == 0 && currentInputMovementVector.y == 0 && movement.x == 0 && movement.y == 0) return;
         currentInputMovementVector = Vector2.SmoothDamp(currentInputMovementVector, movement, ref smoothInputVelocity, smoothInputSpeed);
-        // mainCamera.transform.localPosition = new Vector3()
-
+        mainCamera.transform.Translate(currentInputMovementVector.x * moveSpeed * Time.deltaTime, 0f, currentInputMovementVector.y * moveSpeed * Time.deltaTime, Space.Self);
     }
 }
